e6d7718df57c2fc6c915fbd262fdf414909b2495
Bhupendr1 <bhupendranirwn737@gmail.com>
2023-11-29 14:58:26 +0530

    29-11-2023

---
+++ src/app/service/user.resolver.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Response } from '../interface/response.interface';
+import { Observable } from 'rxjs';
+import { UserService } from './user.service';
+@Injectable({ providedIn: 'root' })
+
+export class UserResolver implements Resolve<Response>{
+  constructor(private userService: UserService) { }
+  resolve(route: ActivatedRouteSnapshot, _: RouterStateSnapshot): Observable<Response> {
+    return this.userService.getUser(route.paramMap.get('uuid')!);
+  }
+}
